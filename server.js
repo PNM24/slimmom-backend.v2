c2b85f1af64b9dd07fe321749caaef937d9acc1f
Catalin <catalin.es@hotmail.com>
2024-12-06 19:11:45 +0200

    Fix Mongoose warnings and ensure proper PORT usage

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const colors = require("colors");
 
 const PORT = process.env.PORT || 3000;
 
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
 app.listen(PORT, () => {
   console.log(
     colors.bgBlue.italic.bold(`Server is running. Use our API on port: ${PORT}`)
--- utils/connectToDb.js
+++ utils/connectToDb.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectToDb = async () => {
-
   try {
-    await mongoose.connect(process.env.DB_HOST, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_HOST);
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Error connecting to database:", error.message);
-    process.exit(1);
+    process.exit(1); // Oprește aplicația în caz de eroare
   }
 };
 
